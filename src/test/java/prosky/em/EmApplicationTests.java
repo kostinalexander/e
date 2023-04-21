c91558bcb70a9d05a0bb9d2fe4cada65c6f21449
Александр <egorrak3221@mail.ru>
2023-04-21 20:24:40 +0300

    4

---
+++ src/test/java/prosky/em/EmApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
